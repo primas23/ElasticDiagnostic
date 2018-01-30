10b1b7e0f51332e34f2f05590702548f7fe22faa
Primas <primas.petrov@gmail.com>
2018-01-30 23:42:49 +0200

    Added regex

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ElasticDiagnostic
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var json = @"
+            {
+                ""pdts_007"": {},
+                ""pdt_003"": {},
+                ""pdt_002"": {},
+                ""pdts_005"": {},
+                ""pdts_006"": {},
+                ""pdt_001"": {},
+                ""pdts_008"": {},
+                ""pdts_009"": {},                
+                ""pdt_004"": {}
+            }";
+
+            var regex = @"pdt_\d*";
+
+            var matches = Regex
+                    .Matches(json, regex)
+                    .Select(m => m.Value);
+            
+            Console.WriteLine(json);
         }
     }
 }
